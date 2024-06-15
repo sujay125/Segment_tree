3caa1f066e9bfc7f6ecedb0db9d3d40eb178709f
sujay125 <75519272+sujay125@users.noreply.github.com>
2024-06-15 16:10:01 +0530

    Update segment_tree.cpp

--- segment_tree.cpp
+++ segment_tree.cpp
@@ -60,47 +60,9 @@ void solve(){
         cout<<minn<<"\n";
     }
 }
-// void build(int ind, int low, int high, int arr[], int seg[], int orr){
-//     if(low==high){seg[ind]=arr[low]; return;}
-//     int mid=(low+high)/2;
-//     build(2*ind+1, low, mid, arr, seg, !orr);
-//     build(2*ind+2, mid+1, high, arr, seg, !orr);
-
-//     if(orr){seg[ind]=seg[2*ind+1]|seg[2*ind+2];}
-//     else{seg[ind]=seg[2*ind+1]^seg[2*ind+2];}
-// }
-// void update(int ind, int low, int high, int seg[], int orr, int i, int val){
-//     if(low==high){seg[ind]=val; return;}
-//     int mid=(low+high)/2;
-//     if(i<=mid){update(2*ind+1, low,mid, seg,!orr, i, val);}
-//     else{update(2*ind+2, mid+1, high, seg,!orr, i, val);}
-//     if(orr){seg[ind]=seg[2*ind+1]|seg[2*ind+2];}
-//     else{seg[ind]=seg[2*ind+1]^seg[2*ind+2];}
-// }
-// void sol1(){
-//     int n, q; cin>>n>>q;
-//     int el=pow(2,n);
-//     int arr[el];
-//     for(auto &b: arr){cin>>b;}
-//     int seg[4*el];
-
-//     if(n%2==0){build(0,0,el-1,arr, seg, 0);}
-//     else{build(0,0,el-1, arr,seg, 1);}
-
-//     while(q--){
-//         int i, val; cin>>i>>val;
-//         i--;
-//         if(n%2==0){update(0,0,el-1,seg, 0,i,val);}
-//         else{
-//             update(0,0,el-1,seg,1,i,val);
-//         }
-//         cout<<seg[0]<<"\n";
-//     }
-// }
 
 int main()
 {  
     solve();
-    // sol1();
     return 0;
 }
